ef9098215d1d971a02dbeffaf35a01c698bd5b37
Kavya Venky <36632282+KavyaVenky@users.noreply.github.com>
2023-05-29 02:02:19 -0400

    added more files

---
+++ src/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js'
+
+var options = {
+  strings: ["Software Engineer", 'Team Player', "Problem Solver"],
+  typeSpeed: 120,
+  backSpeed: 50,
+  backDelay: 1500,
+  loop: true,
+}
+
+var typed = new Typed('#headline', options)
